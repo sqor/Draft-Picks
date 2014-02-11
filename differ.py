c14260c72c52da467d8f99f17f1bc216613136c6
Jason <bjason@gmail.com>
2014-02-11 09:16:45 -0800

    Simple fetching.

--- differ.py
+++ differ.py
@@ -16,16 +16,26 @@ def readJsonFromFile(fileName):
 def setupDirectory():
     # Create a directory for our data
     reportDir = tempfile.mkdtemp(prefix="report_", dir=".")
-    #Fetch our little pages
+    return reportDir
+
+def generatePageScreenshotPath(index, reportDir):
+    return reportDir + "/" + str(index) + ".png"
 
 def fetchPage(index, url, reportDir):
-    os.popen("./getPage.sh " + url + " " + reportDir + "/" + index)
+    imagePath = generatePageScreenshotPath(index, reportDir )
+    print "--------------------_"
+    print imagePath
+    print url
+    print "--------------------_"
+    os.popen("./getPage.sh " + url + " " + imagePath)
 
 
 def generatePages(pages, siteBase01, siteBase02):
+    '''
+    Takes the pages and adds a base to each page.
+    '''
     pagesWithBase01 = []
     pagesWithBasae02 = []
-    # pdb.set_trace()
     for pagesEntry in pages:
         page01End = pagesEntry["url1"]
         page02End = pagesEntry["url2"]
@@ -35,10 +45,44 @@ def generatePages(pages, siteBase01, siteBase02):
 
     return (pagesWithBase01, pagesWithBasae02)
 
+def fetchPages(reportDir, pages):
+    index = 0
+    for page in pages:
+        fetchPage(index, page, reportDir)
+        print index
+        index+=1
+
 def run(fileName="sites.json"):
+    '''
+    Runs the program
+    '''
+    print "Start"
     # We get our pages into a nicer form
     sites = readJsonFromFile(fileName)
-    (pages01, pages02) = generatePages(sites['pages'], sites["siteBase01"], sites["siteBase02"])
+    (pages01, pages02) = generatePages(
+                sites['pages']
+            ,   sites["siteBase01"], sites["siteBase02"])
     print pages01
     print "pages 02"
     print pages02
+
+    reportDir = setupDirectory()
+    print reportDir
+    fetchPages(reportDir, pages01)
+    print "done Fetching ..."
+    # fetchPages(reportDir, pages02)
+
+# Finally we run our stuff
+run()
+
+
+
+
+
+
+
+
+
+
+
+
